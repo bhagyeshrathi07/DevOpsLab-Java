5206c030851b2ec76955fef8f8a361e461d9dcd4
BhagyeshRathi <Bhageyesh2161@gmail.com>
2024-04-24 19:00:54 -0700

    updated yaml, nameChecker.java

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -11,8 +11,33 @@ public class NameChecker {
      * @return True if input is a valid name, else false
      */
     public static boolean check(String input) {
-        // TODO: implement
+        // Check length
+        if (input.length() < 2 || input.length() > 40) {
+            return false;
+        }
 
-        return false;
+        // Check for invalid starting character
+        char firstChar = input.charAt(0);
+        if (!Character.isLetter(firstChar) && firstChar != '\'' && firstChar != '-') {
+            return false;
+        }
+
+        // Check for consecutive hyphens or single quotes
+        for (int i = 0; i < input.length() - 1; i++) {
+            char currentChar = input.charAt(i);
+            char nextChar = input.charAt(i + 1);
+            if ((currentChar == '-' || currentChar == '\'') && (nextChar == '-' || nextChar == '\'')) {
+                return false;
+            }
+        }
+
+        // Check for invalid characters
+        for (char c : input.toCharArray()) {
+            if (!Character.isLetter(c) && c != '\'' && c != '-') {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
